e4c0566acd760e6f8b22f6ef33b074de229671f3
Magnus Deininger <magnus@ef.gy>
2014-09-11 02:20:16 +0400

    started writing some functions to deal with measurement protocol uploads

--- tail-log.js
+++ tail-log.js
@@ -1,10 +1,16 @@
 var tail = require('./lib/tail').tail,
-    analyse = require('./lib/analyse').analyse;
+    analyse = require('./lib/analyse').analyse,
+    mp = require('./lib/measurement-protocol');
 var files = process.argv.slice(2);
+
+var context = mp.context('frob');
 
 for (i in files) {
   tail(files[i],
     function (line) {
-      console.log(analyse(line));
+      var analysis = analyse(line);
+      console.log(analysis);
+      console.log(mp.convert(analysis));
+      context.post(mp.convert(analysis));
     });
 }
