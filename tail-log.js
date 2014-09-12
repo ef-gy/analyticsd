5aa845a5617115bfb2bbad6f1b471c6c4a8d9097
Magnus Deininger <magnus@ef.gy>
2014-09-13 01:14:18 +0400

    only parse new log lines (as opposed to all of them); turn the main script into a proper executable one

--- tail-log.js
+++ tail-log.js
@@ -1,15 +1,21 @@
+#!/usr/bin/env nodejs
+
 var tail = require('./lib/tail').tail,
     analyse = require('./lib/analyse').analyse,
     mp = require('./lib/measurement-protocol');
-var files = process.argv.slice(2);
 
-var context = mp.context('frob');
+if (process.argv.length < 3) {
+    console.log(process.argv[1] + ' TID [log...]');
+    return;
+}
+
+var files = process.argv.slice(3),
+    context = mp.context(process.argv[2]);
 
 for (i in files) {
   tail(files[i],
     function (line) {
       var analysis = analyse(line);
-      console.log(analysis);
       context.post(mp.convert(analysis));
     });
 }
