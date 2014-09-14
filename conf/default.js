bdac6ff4f56e41e679dd2dd1afa2a543ede09de2
Magnus Deininger <magnus@ef.gy>
2014-09-14 14:07:26 +0400

    provide some metadata for session closes; turn the mapping between intermediary analysis and measurement protocol variable names into something configurable - and add default configuration elements for that

--- conf/default.js
+++ conf/default.js
@@ -1,5 +1,17 @@
 module.exports = {
   'tail': ['/var/log/auth.log', '/var/log/daemon.log'],
   'tid': null,
-  'endpoint': 'https://ssl.google-analytics.com/collect'
+  'endpoint': 'https://ssl.google-analytics.com/collect',
+  'payload': {
+    'map': {
+      'session': 'cid',
+      'control': 'sc',
+      'address': 'uip',
+      'type': 't',
+      'category': 'ec',
+      'action': 'ea',
+      'label': 'el',
+      'page': 'p'
+    }
+  }
 };
